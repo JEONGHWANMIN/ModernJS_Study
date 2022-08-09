2f782dba7d60199a1681bff5bbb5663a4115210d
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-09 19:44:44 +0900

    Docs : "추가 Chapter24.md "

--- hwanmin/test.js
+++ hwanmin/test.js
@@ -1,12 +1,11 @@
-const x = 1;
+const funcs = [];
 
-function outer() {
-  const x = 10;
-  const inner = function () {
-    console.log(x);
+for (let i = 0; i < 3; i++) {
+  funcs[i] = function () {
+    return i;
   };
-  return inner;
 }
 
-const innerFunc = outer();
-innerFunc();
+for (let i = 0; i < funcs.length; i++) {
+  console.log(funcs[i]()); // 0 1 2
+}
